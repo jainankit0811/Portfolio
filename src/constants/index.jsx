759f194e72ad4dae7a878148c4ce803415ab2048
jainankit08 <ankitd0811@gmail.com>
2025-05-30 06:21:41 +0530

    new update

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,9 +1,9 @@
 import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 
-export const HERO_CONTENT = `I am a third-year Computer Science Engineering student with expertise in prompt engineering and development.As a front-end developer, I played a key role in a national hackathon runner-up team, building innovative projects from the ground up, including a React-Express application. I excel in solving problems, managing projects, and delivering impactful tech solutions with precision and creativity.`;
+export const HERO_CONTENT = `I am a final-year Computer Science Engineering student with a strong foundation in software development and engineering principles. As a Software Developer, I played a key role in a national hackathon runner-up team, where I contributed to building full-stack applications using technologies like React and Express. I specialize in developing scalable and user-focused solutions, managing end-to-end project lifecycles, and solving complex technical challenges with precision and creativity.`;
 
-export const ABOUT_TEXT = `I am a dedicated third-year Computer Science Engineering student at Techno India NJR Institute of Technology, Udaipur, with a strong focus on software development, prompt engineering, and creating innovative tech solutions. Over the years, I've gained valuable experience by taking on challenging roles, including contributing to a national hackathon runner-up team and developing impactful, user-centric projects.
+export const ABOUT_TEXT = `I am a dedicated third-year Computer Science Engineering student at Techno India NJR Institute of Technology, Udaipur, with a strong focus on software development, Artificial intelligence, and creating innovative tech solutions. Over the years, I've gained valuable experience by taking on challenging roles, including contributing to a national hackathon runner-up team and developing impactful, user-centric projects.
 
 I am passionate about building intuitive solutions in web development, and I have expertise in technologies like React.js, Node.js, Python, and Flutter. Some of my notable projects include Bookflow, an efficient library management system that significantly enhanced student accessibility, and TechieAI, an AI-powered platform that revolutionizes PC building with real-time, personalized hardware recommendations.
 
@@ -11,25 +11,40 @@ With a relentless drive for continuous learning, I thrive in dynamic environment
 
 export const EXPERIENCES = [
   {
+    year: "February 2025 – March 2025",
+    role: "Frontend Developer",
+    company: "Freelance (Remote)",
+    description: `Developed scalable, responsive web apps with React.js and Tailwind; enhanced performance and boosted user engagement.`,
+    technologies: ["React.js", "Tailwind CSS"],
+  },
+  {
     year: "April 2024 - May 2024",
-    role: "CSEdge Verified Certificate for Python Programming Internship",
+    role: "Python Programming Internship",
     company: "CSEdge",
     description: `Thrilled to complete a one-month internship in Python Programming at CSEdge, mastering coding intricacies and gaining confidence in my abilities under expert guidance.`,
     technologies: ["Python programming language"],
+
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "BookFlow",
+    title: "Foodverse - Recipe Website",
     image: project1,
+    description:
+      "Developed a recipe website featuring diet-based filtering and search functionality to enhance usability. Leveraged HTML, CSS, and JavaScript to create an engaging user experience, resulting in improved efficiency and higher user engagement.",
+    technologies: ["HTML", "CSS", "JavaScript", "API Development"],
+  },
+  {
+    title: "BookFlow",
+    image: project2,
     description:
       "BookFlow is a library management app allowing users to manage book borrowing and returns using QR code scanning seamlessly. This app aims to provide an efficient way to interact with library resources without requiring direct librarian involvement.",
     technologies: ["React", "Node.js", "Express", "MongoDB", "Netlify", "Github"],
   },
   {
     title: "Techie AI",
-    image: project2,
+    image: project3,
     description:
       "TechieAi is a Flutter-powered app that transforms the PC building process by cutting out intermediaries, allowing users to find top-quality components at optimal prices. Leveraging advanced AI and the Gemini API, TechieAi offers real-time, precise recommendations for selecting compatible hardware, guiding users from CPU and GPU selection to ensuring compatibility across all parts. With its intuitive interface, TechieAi’s PC Build Chatbot enables users to make well-informed decisions, ensuring a high-performance, budget-friendly PC build every time.",
     technologies: ["Flutter", "Google Gemini", "Firebase", "Mobile Application Development"],
@@ -37,7 +52,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "K-156, R.H.B. Colony, Sector-14, Goverdhan Vilas in Udaipur, Rajasthan - 313001",
+  address: "Sector-14, Goverdhan Vilas in Udaipur, Rajasthan - 313001",
   phoneNo: "+91 9079932431 ",
   email: "ankitd0811@gmail.com",
 };
